bf2522f6f894d1d30c8056843a297e7f8d44589f
slowjo <fabiansen.s83@gmail.com>
2024-02-21 18:26:29 +0200

    change like button position

--- components/HorizontalCard.tsx
+++ components/HorizontalCard.tsx
@@ -23,12 +23,12 @@ export default async function HorizontalCard({ post } : { post : PrismicDocument
         <div className="md:col-span-6 md:col-start-7 p-4 flex gap-5 flex-col items-center bg-white rounded-lg md:flex-row dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 relative fade-in">
             <Link href={`/${post.uid}`} className="relative h-48 w-full md:h-full overflow-clip rounded-lg md:basis-full">
                 <Image fill={true} placeholder="blur" blurDataURL={blurredImage} src={post.data.preview_image.url || ''} alt={post.data.preview_image.alt || ''} className="rounded-lg object-cover md:absolute inset-0 max-h-56 md:max-h-none min-w-full min-h-full w-full h-48 md:w-auto md:h-auto hover:scale-110 transition-transform duration-1000" />
-                <span className="bg-white text-gray-800 text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-400 absolute top-5 right-5">
+                {/* <span className="bg-white text-gray-800 text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-400 absolute top-5 right-5">
                     <HiStar />
                     {count && (
                         <p>{count}</p>
                     )}
-                </span>
+                </span> */}
             </Link>
             <div className="flex flex-col justify-between leading-normal h-fit md:basis-full">
                 <Link href={`/${post.uid}`}>
@@ -38,7 +38,7 @@ export default async function HorizontalCard({ post } : { post : PrismicDocument
                 <div className="flex gap-5">
                     <MarkButton postId={post.id} isMarked={bookmarkData ? true : false} guest={guest} markAs={'bookmark'} />
                     {/* <p className="text-sm text-slate-300">Bookmarked: {bookmarkData ? 'Yes' : 'No'}</p> */}
-                    {/* <MarkButton postId={post.id} isMarked={data ? true : false} guest={guest} markAs={'like'} /> */}
+                    <MarkButton postId={post.id} isMarked={data ? true : false} guest={guest} markAs={'like'} count={count} />
                     {/* <p className="text-sm text-slate-300">Liked: {data ? 'Yes' : 'No'}</p> */}
                 </div>
             </div>
--- components/VerticalCard.tsx
+++ components/VerticalCard.tsx
@@ -29,22 +29,22 @@ export default async function VerticalCard({ post } : { post : PrismicDocumentWi
         <div className="p-4 mb-4 md:mb-0 flex flex-col md:col-span-6 md:row-span-3 bg-orange-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 fade-in">
             <Link href={`/${post.uid}`} className="relative h-56 w-full md:h-full overflow-clip rounded-lg">
                 <Image priority fill={true} placeholder="blur" blurDataURL={blurredImage} className="rounded-lg md:absolute inset-0 md:min-h-full min-w-full hover:scale-105 transition-transform duration-1000 object-cover max-h-56 md:max-h-none w-full h-56 md:w-auto md:h-auto" src={post.data.preview_image.url || ''} alt={post.data.preview_image.alt || ''} />
-                <span className="bg-white text-gray-800 text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-400 absolute top-5 right-5">
+                {/* <span className="bg-white text-gray-800 text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-400 absolute top-5 right-5">
                     <HiStar />
                     {count && (
                         <p>{count}</p>
                     )}
-                </span>
+                </span> */}
             </Link>
             <div className="p-5">
                 <Link href="{`/${post.uid}`}">
                     <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{post.data.title}</h3>
                 </Link>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{post.data.preview_text}</p>
-                <div className="flex justify-between md:justify-start md:gap-5">
+                <div className="flex justify-between md:justify-start items-center md:gap-5">
                     <MarkButton postId={post.id} isMarked={bookmarkData ? true : false} guest={guest} markAs={'bookmark'} />
                     {/* <p className="text-sm text-slate-300">Bookmarked: {bookmarkData ? 'Yes' : 'No'}</p> */}
-                    {/* <MarkButton postId={post.id} isMarked={data ? true : false} guest={guest} markAs={'like'} /> */}
+                    <MarkButton postId={post.id} isMarked={data ? true : false} guest={guest} markAs={'like'} count={count} />   
                     {/* <p className="text-sm text-slate-300">Liked: {data ? 'Yes' : 'No'}</p> */}
                 </div>
             </div>
