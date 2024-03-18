4813ccd38221b69a8fc8ced764a3492fbac7cce1
slowjo <fabiansen.s83@gmail.com>
2024-03-18 09:30:40 +0200

    remove unused code from article

--- app/[uid]/page.tsx
+++ app/[uid]/page.tsx
@@ -1,51 +1,19 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
-import { JSXMapSerializer, PrismicRichText, SliceZone } from "@prismicio/react";
-
 import { createClient } from "@/prismicio";
 // import { components } from "@/slices";
-import { getPostLikes, getUserPostBookmark, getUserPostLike } from "../actions";
-import MarkButton from "@/components/MarkButton";
-import Image from "next/image";
 import Article from "@/components/Article";
 import { Suspense } from "react";
-import HorizontalLoadingCard from "@/components/HorizontalLoadingCard";
+// import HorizontalLoadingCrd from "@/components/HorizontalLoadingCard";
 
 type Params = { uid: string };
 
-// const components: JSXMapSerializer = {
-//   image: ({ node }) => (
-//     <Image src={node.url} width={node.dimensions.width} height={node.dimensions.height} alt={node.alt || ''} className="fade-in" />
-//   )
-// }
-
 export default function Page({ params }: { params: Params }) {
-    // const client = createClient();
-    // const page = await client
-    // .getByUID("post", params.uid)
-    // .catch(() => notFound());
-
-    // const { data, error, guest } = await getUserPostLike(page.id)
-
-    // const { count, error : allLikesError } = await getPostLikes(page.id)
-
-    // const { data : bookmarkData, error : bookMarkError, guest : bookMarkGuest } = await getUserPostBookmark(page.id)
-
-//   return <SliceZone slices={page.data.slices} components={components} />;
-
     return (
         <main className="p-5 md:p-20">
-          <Suspense fallback={<div className="grid mx-auto max-w-lg"><HorizontalLoadingCard /></div>}>
+          {/* <Suspense fallback={<div className="grid mx-auto max-w-lg"><HorizontalLoadingCard /></div>}>
             <Article articleUid={params.uid} />
-          </Suspense>
-            {/* <article className="prose prose-img:rounded-xl mx-auto">
-                <div className="not-prose flex justify-end gap-5">
-                    <MarkButton postId={page.id} isMarked={bookmarkData ? true : false} guest={guest} markAs={'bookmark'} />
-                    <MarkButton postId={page.id} isMarked={data ? true : false} guest={guest} markAs={'like'} count={count} />
-                </div>
-                <h1>{page.data.title}</h1>
-                <PrismicRichText field={page.data.textandimages} components={components} />
-            </article> */}
+          </Suspense> */}
         </main>
     )
 }
---
+++ components/ArticleLoadingState.tsx
@@ -0,0 +1,10 @@
+export default function ArticleLoadingState() {
+    return (
+        <div className="w-full max-w-lg">
+            <div className="h-4 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+            <div className="h-4 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-4"></div>
+        </div>
+    )
+}
