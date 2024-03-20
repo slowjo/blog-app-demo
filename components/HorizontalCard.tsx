6a66fd58d0da13495382675b0d65a78eb3681a9e
slowjo <fabiansen.s83@gmail.com>
2024-03-20 13:14:11 +0200

    remove unused code in cards

--- components/HorizontalCard.tsx
+++ components/HorizontalCard.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import { HiOutlineBookmark, HiOutlineStar, HiStar } from 'react-icons/hi';
 import { PrismicDocumentWithUID } from "@prismicio/client";
 import { PostDocumentData, Simplify } from "@/prismicio-types";
 import getImage from "@/utils/getImage";
 import CardImage from "@/components/CardImage";
 import MarkButtonDataWrapper from "@/components/MarkButtonDataWrapper";
+import { Suspense } from "react";
 
 
 export default async function HorizontalCard({ post } : { post : PrismicDocumentWithUID<Simplify<PostDocumentData>, "post", string> }) {
@@ -16,12 +16,6 @@ export default async function HorizontalCard({ post } : { post : PrismicDocument
         <li className="md:col-span-6 md:col-start-7 p-4 flex gap-5 flex-col items-center bg-white rounded-lg md:flex-row dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 relative fade-in">
             <Link href={`/${post.uid}`} className="relative h-48 w-full md:h-full overflow-clip rounded-lg md:basis-full">
                 <CardImage base64={base64} cardType="horizontal" src={post.data.preview_image.url || ""} alt={post.data.preview_image.alt || ""} width={post.data.preview_image.dimensions?.width || 200} height={post.data.preview_image.dimensions?.height || 200} />
-                {/* <span className="bg-white text-gray-800 text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-400 absolute top-5 right-5">
-                    <HiStar />
-                    {count && (
-                        <p>{count}</p>
-                    )}
-                </span> */}
             </Link>
             <div className="flex flex-col justify-between leading-normal h-fit md:basis-full">
                 <Link href={`/${post.uid}`}>
@@ -29,12 +23,10 @@ export default async function HorizontalCard({ post } : { post : PrismicDocument
                 </Link>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{post.data.preview_text}</p>
                 <div className="flex gap-5">
-                    <MarkButtonDataWrapper postId={post.id} markAs="bookmark" />
-                    <MarkButtonDataWrapper postId={post.id} markAs="like" />
-                    {/* <MarkButton postId={post.id} isMarked={bookmarkData ? true : false} guest={guest} markAs={'bookmark'} /> */}
-                    {/* <p className="text-sm text-slate-300">Bookmarked: {bookmarkData ? 'Yes' : 'No'}</p> */}
-                    {/* <MarkButton postId={post.id} isMarked={data ? true : false} guest={guest} markAs={'like'} count={count} /> */}
-                    {/* <p className="text-sm text-slate-300">Liked: {data ? 'Yes' : 'No'}</p> */}
+                    <Suspense fallback={<div>loading...</div>}>
+                        <MarkButtonDataWrapper postId={post.id} markAs="bookmark" />
+                        <MarkButtonDataWrapper postId={post.id} markAs="like" />
+                    </Suspense>
                 </div>
             </div>
         </li>
--- components/VerticalCard.tsx
+++ components/VerticalCard.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import { HiOutlineBookmark, HiOutlineStar, HiStar } from 'react-icons/hi';
 import { PrismicDocumentWithUID } from "@prismicio/client";
 import { PostDocumentData, Simplify } from "@/prismicio-types";
 import getImage from "@/utils/getImage";
 import CardImage from '@/components/CardImage';
 import MarkButtonDataWrapper from "./MarkButtonDataWrapper";
+import { Suspense } from "react";
 
 
 export default async function VerticalCard({ post } : { post : PrismicDocumentWithUID<Simplify<PostDocumentData>, "post", string> }) {
@@ -14,12 +14,6 @@ export default async function VerticalCard({ post } : { post : PrismicDocumentWi
         <li className="p-4 mb-4 md:mb-0 flex flex-col md:col-span-6 md:row-span-3 bg-orange-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 fade-in">
             <Link href={`/${post.uid}`} className="relative h-56 w-full md:h-full overflow-clip rounded-lg">
                 <CardImage base64={base64} cardType="vertical" src={post.data.preview_image.url || ""} alt={post.data.preview_image.alt || ""} width={post.data.preview_image.dimensions?.width || 200} height={post.data.preview_image.dimensions?.height || 200} />
-                {/* <span className="bg-white text-gray-800 text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-400 absolute top-5 right-5">
-                    <HiStar />
-                    {count && (
-                        <p>{count}</p>
-                    )}
-                </span> */}
             </Link>
             <div className="p-5">
                 <Link href="{`/${post.uid}`}">
@@ -27,12 +21,10 @@ export default async function VerticalCard({ post } : { post : PrismicDocumentWi
                 </Link>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{post.data.preview_text}</p>
                 <div className="flex items-center gap-5">
-                    <MarkButtonDataWrapper postId={post.id} markAs="bookmark" />
-                    <MarkButtonDataWrapper postId={post.id} markAs="like" />
-                    {/* <MarkButton postId={post.id} isMarked={bookmarkData ? true : false} guest={guest} markAs={'bookmark'} /> */}
-                    {/* <p className="text-sm text-slate-300">Bookmarked: {bookmarkData ? 'Yes' : 'No'}</p> */}
-                    {/* <MarkButton postId={post.id} isMarked={data ? true : false} guest={guest} markAs={'like'} count={count} />    */}
-                    {/* <p className="text-sm text-slate-300">Liked: {data ? 'Yes' : 'No'}</p> */}
+                    <Suspense fallback={<div>loading...</div>}>
+                        <MarkButtonDataWrapper postId={post.id} markAs="bookmark" />
+                        <MarkButtonDataWrapper postId={post.id} markAs="like" />
+                    </Suspense>
                 </div>
             </div>
         </li>
