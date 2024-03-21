5e7bc9ece6ece52b58db3e221c214889ea5608fb
slowjo <fabiansen.s83@gmail.com>
2024-03-21 12:39:09 +0200

    add padding to article loader

--- components/ArticleLoadingState.tsx
+++ components/ArticleLoadingState.tsx
@@ -1,6 +1,6 @@
 export default function ArticleLoadingState() {
     return (
-        <div className="w-full max-w-2xl mx-auto animate-pulse">
+        <div className="w-full max-w-2xl mx-auto animate-pulse p-5 md:p-20">
             <div className="h-7 bg-gray-200 rounded-full dark:bg-gray-700 mr-5 mb-9"></div>
             <div className="h-64 bg-gray-300 rounded-lg dark:bg-gray-700 mb-7"></div>
             <div className="h-4 bg-gray-200 rounded-full dark:bg-gray-700 w-56 mb-7"></div>
