cabaa75a8f0382503d3a8cc345ee71dd2198caeb
slowjo <fabiansen.s83@gmail.com>
2024-03-24 13:36:56 +0200

    change markbutton loader style

--- components/MarkButtonLoadingState.tsx
+++ components/MarkButtonLoadingState.tsx
@@ -1,8 +1,20 @@
+import { Button } from "flowbite-react";
+import { HiOutlineBookmark, HiOutlineStar } from "react-icons/hi";
+
 export default function MarkButtonLoadingState() {
     return (
-        <div className="flex gap-5 animate-pulse">
-            <div className="h-10 w-10 bg-gray-300 rounded-full" />
-            <div className="h-10 w-10 bg-gray-300 rounded-full" />
+        // <div className="flex gap-5 animate-pulse">
+        //     <div className="h-10 w-10 bg-gray-300 rounded-full" />
+        //     <div className="h-10 w-10 bg-gray-300 rounded-full" />
+        // </div>
+        <div className="flex gap-5 text-gray-500 animate-pulse">
+            <Button disabled size="lg" className="bg-transparent text-gray-500 scale-95 w-10 h-10 rounded-full">
+                <HiOutlineBookmark className="text-lg" />
+            </Button>
+            <Button disabled size="lg" className="bg-transparent text-gray-500 scale-95 w-10 h-10 rounded-full">
+                <HiOutlineStar className="text-lg" />
+                <div className="ml-1">-</div>
+            </Button>
         </div>
     )
 }
